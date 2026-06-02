ef057032f33f97286ad557be881d7b7f05890081
Otávio Martineschen <otaviomart7tds@gmail.com>
2026-06-02 11:02:33 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
-import './App.module.css'
+import s from './App.module.css'
 import { useEffect, useState } from 'react'
 import { api } from './constants/api'
 
+import logo from '/logo.png'
+
 function App() {
   const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+  
 
   useEffect(() => {
-    api.get('/character').then((response) => {
+    api.get(`/character/?page = ${page}`).then((response) => {
         setData(response.data.results)
     }).catch((error) => {
-      console.error('Deu Ruim')
+      console.error("Deu ruim!!!", error)
     })
-
-  }, [])
+  }, [page])
   
 
   return (
     <>
-      <img src="" alt="" />
+      <img className={s.logo} src={logo} alt="Logo" />
+      <div>
+      <label>Digite uma Página:</label>
+      <input min={1} max={42} type="number" placeholder='1/42' value={page} onChange={(e) => setPage(parseInt(e.target.value))}/>
+    </div>
       <main>
-        {data.nap((item, index) => {
+        {data.map(item => {
           return(
-            <div key = 'index'>
-              <img src={item.image} alt={item.image} />
-              <h3>{item.name}</h3>
-              <p>Species: {item.species}</p>
-              {item.status === "Dead" ? "Status: ☠️" : item.status === "Alive" ? "Status: ❤️" : <p>Status: {item.status}</p> }
-              <p>Origin: {item.origin.name}</p>
+            <div key={item.id}>
+                <img src={item.image} alt={item.name} />
+                <h4>Name: {item.name}</h4>
+                <p>Species: {item.species}</p>
+                {item.status === "Dead" ? "Status:🧟" : item.status === "Alive" ? "Status: 😊" : <p>Status: {item.status}</p>}
+                <p>Origin: {item.origin.name}</p>
             </div>
           )
         })}
@@ -34,6 +41,5 @@ function App() {
     </>
   )
 }
-
 
 export default App
